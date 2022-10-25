bae54e9b5fba315a84919b161ebf6defcae5b5c8
Pablo Juárez <87239199+psebas99@users.noreply.github.com>
2022-10-25 17:53:21 -0600

    Hola

--- decode.py
+++ decode.py
@@ -1,19 +1,20 @@
 from operator import xor
+import string
 
 aux = int
 
-d1=int
-d2=int
-d3=int
-d4=int
+d1 = int
+d2 = int
+d3 = int
+d4 = int
 
 p1 = int
 p2 = int
 p3 = int
 
-p11=int
-p12=int
-p13=int
+p11 = int
+p12 = int
+p13 = int
 
 
 e1 = int
@@ -30,28 +31,74 @@ mensaje1 = str('')
 
 # archivo1=open('Mensajes_incorrectos.txt','w')
 # archivo2=open('Mensajes_correctos.txt','w')
-#texto = ['0101010', '1010101', '1110000', '0001111', '0000000']
+texto = []
+tren= ""
 palabra = ['0101010', '1010101', '1110000', '0001111', '0000000']
 
 for j in palabra:
 
-    for i in j:
-        if i == 0:
-            p1 = int(j[0])
-        if i == 1:
-            p2 = int(j[1])
-        if i == 2:
-            d1 = int(j[2])
-        if i == 3:
-            p3 = int(j[3])
-        if i == 4:
-            d2 = int(j[4])
-        if i == 5:
-            d3 = int(j[5])
-        if i == 6:
-            d4 = int(j[6])
+    #print(j[0]+j[1]+j[2]+j[3]+j[4]+j[5]+j[6])
 
-    print(str(p1))
+    p1 = j[0]
+    p2 = j[1]  
+    d1 = j[2] 
+    p3 = j[3]  
+    d2 = j[4]
+    d3 = j[5]
+    d4 = j[6]
+
+    #print(p1+p2+d1+p3+d2+d3+d4)
+
+    p11=xor(int(d1),int(d2))
+    p11=xor(p11,int(d4))
+    p22=xor(int(d1),int(d3))
+    p22=xor(p22,int(d4))
+    p33=xor(int(d2),int(d3))
+    p33=xor(p33,int(d4))
+
+    e1=xor(int(p1),p11)
+    e2=xor(int(p2),p22)
+    e3=xor(int(p3),p33)
+    er=(4*e3)+(2*e2)+(1*e1)
+    #print(er)
+
+
+
+    if er==3:
+        if d1==1:d1=0
+        else: d1=1
+
+    if er==5:
+        if d2==1:d2=0
+        else: d2=1
+
+    if er==6:
+        if d3==1:d3=0
+        else: d3=1
+
+    if er==7:
+        if d4==1:d4=0
+        else: d4=1   
+    
+    #print(p1+p2+d1+p3+d2+d3+d4)
+    
+    word = d1+d2+d3+d4
+
+    #print(word)
+
+    texto.append(word)
+    tren = str(tren) + str(word)
+
+#print(texto)
+print(tren)
+
+
+
+
+ 
+
+     
+
 
 
 
